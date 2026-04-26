c169b89547b150f8a1f1d2ae3872d8e6ca8b57ae
Dewang <dgspam3@gmail.com>
2026-04-26 13:28:27 +0530

    config changes

--- src/config/audit.ts
+++ src/config/audit.ts
@@ -6,4 +6,4 @@ export const AUDIT_PRICE_USD = 250;
 
 export const PAYPAL_LINK = 'https://www.paypal.com/ncp/payment/PHAQHJKXEBN52';
 export const CALENDLY_CALL_URL = 'https://calendly.com/dewanggogte/30min';
-export const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSdt1DqSZ-Wtu7dWCxIM8Gh7w6wbVmpQ3n_6dZmuqRXsJ3nDjQ/viewform';
+export const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScR0fFokut5cnwJl3Kb0IbP4F5a7QJXKrN0YqKNsBYfQ56JHw/viewform';
